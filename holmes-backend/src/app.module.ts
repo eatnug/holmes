9bd8ad5eb9202b32184d43e038a3b405c2581be6
eatnug <wlq4568@gmail.com>
2024-08-06 01:10:20 +0900

    add client

--- holmes-backend/src/app.module.ts
+++ holmes-backend/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
 import { HouseModule } from './houses/house.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, HouseModule],
+  imports: [
+    UserModule,
+    HouseModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
